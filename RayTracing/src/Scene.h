836f97bc4b5dcede378dec768001f4ffb3b4ec72
pshrey-29 <shrey29303@gmail.com>
2023-03-10 23:28:48 +0530

    Materials
    -Roughness
    -Global Illumination
    -Shadow

--- RayTracing/src/Scene.h
+++ RayTracing/src/Scene.h
@@ -4,12 +4,19 @@
 
 #include <vector>
 
+struct Material
+{
+	glm::vec3 Albedo{ 1.0f };
+	float Roughness = 1.0f;
+	float Metallic = 0.0f;
+};
+
 struct Sphere
 {
 	glm::vec3 Position{ 0.0f };
 	float Radius = 0.5f;
 
-	glm::vec3 Albedo{ 1.0f };
+	int MaterialIndex = 0;
 };
 
 struct Light
@@ -21,5 +28,6 @@ struct Scene
 {
 	int numSpheres = 3;
 	std::vector<Sphere> Spheres;
+	std::vector<Material> Materials;
 	Light lights;
 };
